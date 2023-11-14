287d580d5d51552651f09793b7d23d401f9515e2
Your Name <you@example.com>
2023-11-13 22:00:49 -0300

    missing main()

--- src/Arvore.java
+++ src/Arvore.java
@@ -12,6 +12,20 @@ public class Arvore {
     public Arvore(No raiz) {
         this.raiz = raiz;
     }
+    public Arvore(int conteudo) {
+        No no = new No(conteudo);
+        this.raiz = no;
+    }
+
+    public boolean buscar(int conteudo) {
+        if(buscar(raiz, conteudo) != null) {
+            System.out.println("Chave encontrada!");
+            return true;
+        }
+        System.out.println("Chave não encontrada!");
+        return false;
+
+    }
 
     public No buscar(No raiz, int conteudo) {
         if(raiz == null) {
@@ -33,8 +47,16 @@ public class Arvore {
     }
 
     public No inserir(No raiz, int conteudo) {
+        if(this.raiz == null) {
+            No aux = new No(conteudo);
+            this.raiz = aux;
+            System.out.println(conteudo + " adicionado!");
+            return raiz;
+        }
         if (raiz == null) {
-            raiz = new No(conteudo);
+            No aux = new No(conteudo);
+            raiz = aux;
+            System.out.println(conteudo + " adicionado!");
             return raiz;
         }
         if (conteudo < raiz.conteudo) {
@@ -42,6 +64,9 @@ public class Arvore {
         }
         else if(conteudo > raiz.conteudo) {
             raiz.noDireita = inserir(raiz.noDireita, conteudo);
+        }
+        else {
+            System.out.println(conteudo + " já está na árvore, não pode ser inserido!");
         }
         return raiz;
     }
@@ -84,30 +109,52 @@ public class Arvore {
             pos_ordem_filhos(no.noEsquerda);
         if(no.noDireita != null) 
             pos_ordem_filhos(no.noDireita);
-        altera_filhos_esquerda(no);
-        altera_filhos_direita(no);
+        no.qtdNosEsquerda = conta_filhos_esquerda(no);
+        no.qtdNosDireita = conta_filhos_direita(no);
     }
+    
+    public int conta_filhos_esquerda(No no) {
+        if(no == null || no.noEsquerda == null)
+            return 0;
 
-    public int altera_filhos_esquerda(No no) {
-        if(no != null && no.noEsquerda != null && no.noDireita != null) {
-            return 1 + altera_filhos_esquerda(no.noEsquerda) + altera_filhos_esquerda(no.noDireita);
+        int filhos = 0;
+        No aux;
+        Stack<No> pilha = new Stack<>();
+        pilha.add(no.noEsquerda);
+
+        while(!pilha.empty()) {
+            aux = pilha.pop();
+            filhos++;
+            
+            if(aux.noEsquerda != null)
+                pilha.add(aux.noEsquerda);
+            if(aux.noDireita != null)
+                pilha.add(aux.noDireita);
         }
-        return 0;
 
-
-
-        // if(no == null || (no.noDireita == null && no.noEsquerda == null))
-        //     return 0;
-        // else 
-        //     no.qtdNosEsquerda = 1 + altera_filhos_esquerda(no.noEsquerda) + altera_filhos_esquerda(no.noDireita);
-        // return no.qtdNosEsquerda;
+        return filhos;
     }
 
-    public void altera_filhos_direita(No no) {
-    //     if(no != null) {
-    //         no.qtdNosDireita = altera_filhos_direita(no.noEsquerda) + altera_filhos_direita(no.noDireita);
-    //     }
-    //     return no.qtdNosDireita;
+    public int conta_filhos_direita(No no) {
+        if(no == null || no.noDireita == null)
+            return 0;
+
+        int filhos = 0;
+        No aux;
+        Stack<No> pilha = new Stack<>();
+        pilha.add(no.noDireita);
+
+        while(!pilha.empty()) {
+            aux = pilha.pop();
+            filhos++;
+            
+            if(aux.noEsquerda != null)
+                pilha.add(aux.noEsquerda);
+            if(aux.noDireita != null)
+                pilha.add(aux.noDireita);
+        }
+
+        return filhos;
     }
     
     public String pre_ordem() {
@@ -134,35 +181,11 @@ public class Arvore {
         return resultado;
     }
     
-    public int enesimoElemento(int n) {
-        int posicao = 0;
-        Queue<No> fila = new LinkedList<>();
-        No aux;
-        int resultado = 0;
-
-        fila.add(raiz);
-
-        while(!fila.isEmpty()) {
-            aux = fila.poll();
-            posicao++;
-
-            if(posicao == n)
-                resultado = aux.conteudo;
-
-            if(aux.noEsquerda != null)
-                fila.add(aux.noEsquerda);
-            if(aux.noDireita != null)
-                fila.add(aux.noDireita);
-        }
-
-        return resultado;
-    }
-
     public double media(int x) {
         No aux = buscar(raiz, x);
 
         Queue<No> fila = new LinkedList<>();
-        double quantidade = 8;
+        double quantidade = aux.qtdNosDireita + aux.qtdNosEsquerda + 1;
         double soma = 0;
 
         if(aux != null) {
@@ -182,9 +205,6 @@ public class Arvore {
     }
 
     public int posicao(int x) {
-        if(buscar(raiz, x) == null)
-            return -1;
-
         Stack<No> pilha = new Stack<>();
         No aux = raiz;
         int posicao = 0;
@@ -208,44 +228,56 @@ public class Arvore {
 
     public void imprimeArvore(int s) {
         if(s == 1)
-            imprime_hifen(raiz);
+            imprime_hifen(raiz, 1);
         else
-            imprime_parenteses(raiz);
+            imprime_parenteses(raiz, true);
     }
 
-    public void imprime_hifen(No no) {
-            if (no == null) {
-                return;
+    public void imprime_hifen(No no, int nivel) {
+        if (no != null) {
+            for(int i = 0; i < nivel - 1; i++) {
+                System.out.print("     ");
             }
-    
-            // Calcular o número de hifens com base no nível
-            int numHifens = no.altura * 4;
-            String linha = " ".repeat(numHifens) + no.conteudo + "-------------";
-    
-            // Imprimir a linha
-            System.out.println(linha);
-    
-            // Chama recursivamente para os filhos esquerdo e direito
-            imprime_hifen(no.noEsquerda);
-            imprime_hifen(no.noDireita);
+            
+            System.out.print(no.conteudo);
+            for(int j=0; j < 11 - nivel; j++) {
+                System.out.print("-----");
+            }
+            System.out.println("");
+            imprime_hifen(no.noEsquerda, nivel + 1);
+            imprime_hifen(no.noDireita, nivel + 1);
         }
+    }
 
-    public void imprime_parenteses(No no) {
+    public void imprime_parenteses(No no, boolean flag) {
         if (no != null) {
             System.out.print("(" + no.conteudo);
-            imprime_parenteses(no.noEsquerda);
-            imprime_parenteses(no.noDireita);
-            System.out.print(")");
+            imprime_parenteses(no.noEsquerda, false);
+            imprime_parenteses(no.noDireita, false);
+            if(flag)
+                System.out.println(")");
+            else
+                System.out.print(")");
         }
     }
 
     public boolean ehCheia() {
-        return raiz.qtdNosDireita + raiz.qtdNosEsquerda + 1 == Math.pow(2, raiz.altura + 1) - 1;
+        if(raiz.qtdNosDireita + raiz.qtdNosEsquerda + 1 == Math.pow(2, raiz.altura) - 1) {
+            System.out.println("A árvore é cheia!");
+            return true;
+        }
+        System.out.println("A árvore não é cheia!");
+        return false;
     }
 
     public boolean ehCompleta() {
-        return (Math.pow(2, raiz.altura - 1) <= raiz.qtdNosDireita + raiz.qtdNosEsquerda + 1) && 
-        (raiz.qtdNosDireita + raiz.qtdNosEsquerda + 1 >= Math.pow(2, raiz.altura) - 1);
+        if(((Math.pow(2, raiz.altura - 1)) <= (raiz.qtdNosDireita + raiz.qtdNosEsquerda + 1)) 
+        && ((raiz.qtdNosDireita + raiz.qtdNosEsquerda + 1) <= (Math.pow(2, raiz.altura) - 1))) {
+            System.out.println("A árvore é completa!");
+            return true;
+        }
+        System.out.println("A árvore não é completa!");
+        return false;
     }
 
     public No minimo_esquerda(No no) {
@@ -256,6 +288,7 @@ public class Arvore {
     
     public No remover(No raiz, int conteudo) {
         if(raiz == null) {
+            System.out.println(conteudo + " não está na árvore, não pode ser removido!");
             return raiz;
         }
         else if(conteudo < raiz.conteudo) {
@@ -278,10 +311,47 @@ public class Arvore {
                 No temp = minimo_esquerda(raiz.noDireita);
                 raiz.conteudo = temp.conteudo;
                 raiz.noDireita = remover(raiz.noDireita, temp.conteudo);
+                System.out.println(conteudo + " removido!");
             }
         }
         atribuir_altura();
         atribuir_filhos();
         return raiz;
+    }
+
+    public int mediana() {
+        if(raiz == null) {
+            System.out.println("Árvore vazia!");
+            return -1;
+        }
+        int total_nos = 1 + raiz.qtdNosDireita + raiz.qtdNosEsquerda;
+        int pos_mediana = total_nos/2 + 1;
+        int aux = total_nos/2;
+        if(total_nos % 2 == 1)
+            return enesimoElemento(pos_mediana);
+        if(enesimoElemento(pos_mediana) > enesimoElemento(aux))
+            pos_mediana = aux;
+        return enesimoElemento(pos_mediana);
+    }
+
+    public int enesimoElemento(int n) {
+        Stack<No> pilha = new Stack<>();
+        No aux = raiz;
+        int posicao = 0;
+
+        while(!pilha.isEmpty() || aux != null) {
+            if(aux != null) {
+                pilha.push(aux);
+                aux = aux.noEsquerda;
+            }
+            else {
+                aux = pilha.pop();
+                posicao++;
+                if(posicao == n)
+                    break;
+                aux = aux.noDireita;
+            }
+        }
+        return aux.conteudo;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,93 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.IOException;
 import org.w3c.dom.views.AbstractView;
 
-public class Main {
+public class Main  {
 
-    public static void main(String[] args) {
-        No no = new No(10);
-        Arvore arvore = new Arvore();
-        arvore.raiz = no;
-        arvore.inserir(5);
-        arvore.inserir(15);
-        arvore.inserir(16);
-        arvore.inserir(7);
-        arvore.inserir(3);
-        arvore.inserir(14);
-        arvore.inserir(9);
-        arvore.inserir(23);
-        arvore.inserir(21);
+    public static void main(String[] args) throws IOException {
 
+        ArrayList<String> arvoreInicial = lerArquivo("C:\\Users\\Giovanna\\Desktop\\quarto-semestre\\EDB2\\implementacao_ABB\\src\\elementos");
+        ArrayList<String> comandos = lerArquivo("src\\comandos");
+        ArrayList<Integer> arvoreInteiros = new ArrayList<>();
+        Arvore arvore = new Arvore(arvoreInteiros.get(1));
+        
+        for(String i : arvoreInicial){
+            arvoreInteiros.add(Integer.parseInt(i));
+        }
+        for(String j : comandos) {
+            String[] separado = j.split(" ");
+            String comando = separado[0];
+            String argumento = "";
+            
+            try {
+                argumento = separado[1];
+            } catch (Exception e) {}
+            
+            argumentoComando(comando, argumento, arvore);
+            }
 
-        //System.out.println(arvore.pre_ordem());
-        //System.out.println(arvore.raiz.altura);
+            return;
+        }
+        public static ArrayList<String> lerArquivo(String nome_do_arquivo) throws IOException{
 
-        arvore.remover(arvore.raiz, 9);
+        File file = new File(nome_do_arquivo);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String linha;
+        ArrayList<String> array_string = new ArrayList<>();
+        
+        while((linha = br.readLine()) != null){
+            array_string.add(linha);
+        }
 
-        // if(arvore.ehCheia())
-        System.out.println(arvore.pre_ordem());
+        br.close();
+        fr.close();
 
-        System.out.println(arvore.raiz.qtdNosEsquerda);
-        return;
+        return array_string;
+        }
+
+    	public static void argumentoComando(String comando, String argumento, Arvore arvore) {
+        switch (comando) {
+            case "INSIRA":
+                arvore.inserir(Integer.parseInt(argumento));
+                break;
+            case "REMOVA":
+                arvore.remover(arvore.raiz, Integer.parseInt(argumento));
+                break;
+            case "BUSCAR":
+                arvore.buscar(Integer.parseInt(argumento));
+                break;
+            case "ENESIMO":
+                System.out.println(arvore.enesimoElemento(Integer.parseInt(argumento)));
+                break;
+            case "POSICAO":
+            	System.out.println(arvore.posicao(Integer.parseInt(argumento)));
+                break;
+            case "MEDIANA":
+            	System.out.println(arvore.mediana());
+                break;
+            case "MEDIA":
+            	System.out.println(arvore.media(Integer.parseInt(argumento)));
+                break;
+            case "CHEIA":
+            	if(arvore.ehCheia()){}
+                break;
+            case "COMPLETA":
+            	if(arvore.ehCompleta()){}
+                break;
+            case "PREORDEM":
+            	System.out.println(arvore.pre_ordem());
+                break;
+            case "IMPRIMA":
+                arvore.imprimeArvore(Integer.parseInt(argumento));
+                break;
+        
+            default:
+                break;
+        }
     }
 }
